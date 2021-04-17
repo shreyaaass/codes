db210fbde791b24a8d74102d6c01972294b84043
shreyaaass <shreyas.10.singh@gmail.com>
2021-04-17 23:48:38 +0530

    17/04/21

---
+++ C++/employeeclass.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Employee
+{
+    int eAge;
+    int eId;
+    char eName[20];
+    int eSal;
+    char eGen;
+    char eDesig[20];
+    int eYoJ;
+
+public:
+    void store()
+    {
+        cin >> eAge;
+        cin >> eId;
+        cin >> eName;
+        //cin >> eSal;
+        cin >> eGen;
+        cin >> eDesig;
+        cin >> eYoJ;
+    }
+    void display()
+    {
+        cout << eAge << endl;
+        cout << eId << endl;
+        cout << eName << endl;
+        //cout << eSal<<endl;
+        cout << eGen << endl;
+        cout << eDesig << endl;
+        cout << eYoJ << endl;
+    };
+    void calSalary()
+    {
+        float x;
+        cout << "Enter basic value" << endl;
+        cin >> x;
+        eSal = (x + .2 * x + .2 * x) - (.02 * x);
+        //executable statement
+        cout << eSal << endl;
+    };
+    void updateSal()
+    {
+        if (eGen == 'M' && eAge > 45 && eYoJ > 2004 && (strcmp(eDesig, "manager") == 0))
+        {
+            eSal = eSal * 1.05;
+            cout << "Updated Sal " << eSal << endl;
+        }
+    };
+};
+int main()
+{
+    Employee e;
+    e.store();
+    e.display();
+    e.calSalary();
+    e.updateSal();
+    return 0;
+}
